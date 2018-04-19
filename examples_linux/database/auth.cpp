c6d0cce1234aaae3c58350892ced77124fb323ea
root <mortenbeuchert@gmail.com>
2018-04-19 10:28:37 +0000

    Implemented signInWithEmailAndPassword method

--- examples_linux/database/auth.cpp
+++ examples_linux/database/auth.cpp
@@ -1,5 +1,4 @@
 #include "auth.h"
-#include "cjson.h"
 #include <string>
 #include <iostream>
 #include <stdio.h>
@@ -12,16 +11,14 @@ Auth::Auth(FirebaseConfig cfg) {
   signInWithEmailAndPassword();
 }
 
-string Auth::signInWithEmailAndPassword() {
-  cout << "sign in with email and password" << endl;
+cJSON * Auth::signInWithEmailAndPassword() {
+  //cout << "sign in with email and password" << endl;
   FILE *fpipe;
 
-  string temp = "curl https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key="+config.apiKey+" -H 'Content-Type: application/json' --data-binary '{\"email\":\""+config.email+"\",\"password\":\""+config.password+"\",\"returnSecureToken\":true}'";
+  string tmp = "curl https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key="+config.apiKey+" -H 'Content-Type: application/json' --data-binary '{\"email\":\""+config.email+"\",\"password\":\""+config.password+"\",\"returnSecureToken\":true}'";
    
-  const char* signInCommand = temp.c_str();
-  char c = 0;  
+  const char* signInCommand = tmp.c_str();
   char msg[2048];
-
 
   if (0 == (fpipe = (FILE*)popen(signInCommand, "r"))) {
     perror("popen() failed");
@@ -29,7 +26,8 @@ string Auth::signInWithEmailAndPassword() {
   }
 
   int i = 0;
-  while(fread(&c, sizeof c, 1, fpipe)) {
+  char c = 0;
+  while (fread(&c, sizeof c, 1, fpipe)) {
     msg[i++] = c;
     //printf("%c", c);
   }
@@ -38,12 +36,24 @@ string Auth::signInWithEmailAndPassword() {
   //cJSON *foodTypes = cJSON_GetArrayItem(0);
 
   //char* response = cJSON_Print(root);
-  
-  const cJSON *idTokenStr =  cJSON_GetObjectItemCaseSensitive(root, "idToken");
-  printf("%s", idTokenStr->valuestring);
+
+  cJSON *reply =  cJSON_GetObjectItemCaseSensitive(root, "idToken");
+  //printf("%s", reply->valuestring);
 
   pclose(fpipe);
 
-  cout << "Authing" << endl;
-  return "";
+  cJSON_Delete(root);
+
+  return reply;
+
+/*
+  //printf("%s", idToken->valuestring);  
+
+  string res(idToken->valuestring);
+
+  //cJSON_Delete(idToken);
+
+  cout << endl << "Auth: " << res << endl;
+  return res;
+*/
 }
--- examples_linux/database/auth.h
+++ examples_linux/database/auth.h
@@ -3,13 +3,8 @@
 
 #include <iostream>
 #include <string>
-
-typedef struct {
-  std::string projectId = "foodwastereduction-6ca48";
-  std::string apiKey = "AIzaSyBxlo2I0gI-2c5nb3w9feXabKInEvVotj8";
-  std::string email = "mortenbeuchert@gmail.com";
-  std::string password = "123456";
-} FirebaseConfig;
+#include "globals.h"
+#include "cjson.h"
 
 class Auth {
   private:
@@ -17,7 +12,7 @@ class Auth {
   public:
     Auth(FirebaseConfig cfg);
     ~Auth() { std::cout << "Auth destructed" << std::endl; };
-    std::string signInWithEmailAndPassword();
+    cJSON * signInWithEmailAndPassword();
 };
 
 #endif
