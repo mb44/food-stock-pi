8a5f58c6fb01933efec6cc0b3f40016a5e1a3966
root <mortenbeuchert@gmail.com>
2018-04-19 10:29:49 +0000

    Add auth token. Change method return type

--- examples_linux/database/firebaseadapter.cpp
+++ examples_linux/database/firebaseadapter.cpp
@@ -2,30 +2,52 @@
 #include <string>
 #include <iostream>
 
-
-//class FirebaseAdapter;
-
 using namespace std;
 
 FirebaseAdapter::FirebaseAdapter() {
   //cout << "Constructor called" << endl;
   FirebaseConfig cfg;
-  cfg.projectId = "foodwastereduction-6ca48";
-  cfg.apiKey = "AIzaSyBxlo2I0gI-2c5nb3w9feXabKInEvVotj8";
-  cfg.email = "mortenbeuchert@gmail.com";
-  cfg.password = "123456";
-
+  //cfg.projectId = "foodwastereduction-6ca48";
+  //cfg.apiKey = "AIzaSyBxlo2I0gI-2c5nb3w9feXabKInEvVotj8";
+  //cfg.email = "mortenbeuchert@gmail.com";
+  //cfg.password = "123456";
   auth = new Auth(cfg);
+
+  authToken = auth->signInWithEmailAndPassword();
+  cout << "FirebaseAdapter: " << authToken->valuestring << endl; 
 }
 
 FirebaseAdapter::~FirebaseAdapter() {
-  //cout << "FirebaseAdapter destructed" << endl;
-  delete auth;
+  // delete auth;
+  // free authToken;
+  // delete jsonReply;
 }
 
-string FirebaseAdapter::getContainerItem(int containerId) {
-  cout << "Firebase test" << endl;
-  return "abc";
+cJSON * FirebaseAdapter::getContainerItem(int containerId) {
+  cout << "Get containerItem" << endl;
+  string tmp = "curl 'https://"+cfg.projectId+".firebaseio.com/containers/"+to_string(containerId)+".json?auth="+authToken->valuestring+"'";
+
+  cout << endl << "Tmp: " << tmp << endl;
+
+  FILE *fpipe;
+
+  const char* getContainerItemCmd = tmp.c_str();
+  char msg[2048];
+
+  if (0 == (fpipe = (FILE*)popen(getContainerItemCmd, "r"))) {
+    perror("popen() failed");
+    exit(1);
+  }
+
+  int i = 0;
+  char c = 0;
+  while (fread(&c, sizeof c, 1, fpipe)) {
+    msg[i++] = c;
+  }
+
+  cJSON *reply = cJSON_Parse(msg);
+
+  return reply;
 }
 
 string FirebaseAdapter::createContainerItem(string containerItem) {
