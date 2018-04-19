9a7011e7dc3e7d16c49e1903246a439e0d05253b
root <mortenbeuchert@gmail.com>
2018-04-19 10:26:49 +0000

    Add header file

---
+++ examples_linux/database/globals.h
@@ -0,0 +1,13 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#include <string>
+
+typedef struct {
+  std::string projectId ="foodwastereduction-6ca48";
+  std::string apiKey = "AIzaSyBxlo2I0gI-2c5nb3w9feXabKInEvVotj8";
+  std::string email = "mortenbeuchert@gmail.com";
+  std::string password = "123456";
+} FirebaseConfig;
+
+#endif
