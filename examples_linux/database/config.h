912c00a7ac889eb789e61fbd4981754eda264b6b
root <mortenbeuchert@gmail.com>
2018-04-25 15:15:32 +0000

    Remove hard-coded values

--- examples_linux/database/config.h
+++ examples_linux/database/config.h
@@ -7,11 +7,15 @@
 #define MAX_CONTAINERS 1000
 #define FIREBASE_REPLY_MAX 4096
 
+#define FIREBASECONFIG_LINE_LENGTH_MAX 200
+#define FIREBASECONFIG_VALUE_LENGTH_MAX 150
+
+
 typedef struct {
-  std::string projectId ="foodwastereduction-6ca48";
-  std::string apiKey = "AIzaSyBxlo2I0gI-2c5nb3w9feXabKInEvVotj8";
-  std::string email = "mortenbeuchert@gmail.com";
-  std::string password = "123456";
+  char projectId[FIREBASECONFIG_VALUE_LENGTH_MAX]; // ="foodwastereduction-6ca48";
+  char apiKey[FIREBASECONFIG_VALUE_LENGTH_MAX]; // = "AIzaSyBxlo2I0gI-2c5nb3w9feXabKInEvVotj8";
+  char email[FIREBASECONFIG_VALUE_LENGTH_MAX]; // = "mortenbeuchert@gmail.com";
+  char password[FIREBASECONFIG_VALUE_LENGTH_MAX]; // = "123456";
 } FirebaseConfig;
 
 #endif
