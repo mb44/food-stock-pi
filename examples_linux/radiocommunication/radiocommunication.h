124ebce17386c53980ce022c815c2d51573a92a7
root <mortenbeuchert@gmail.com>
2018-05-01 14:14:46 +0000

    Change constructor signature. Change member variable type

--- examples_linux/radiocommunication/radiocommunication.h
+++ examples_linux/radiocommunication/radiocommunication.h
@@ -3,33 +3,30 @@
 
 #include "../../RF24.h"
 
-#define MIN_PAYLOAD_SIZE 4
-#define MAX_PAYLOAD_SIZE 32
+#define MIN_PAYLOAD_SIZE 1
+#define MAX_RCV_PAYLOAD_SIZE 7
 #define PAYLOAD_SIZE_INCREMENTS_BY 1
-#define NEXT_PAYLOAD_SIZE 4
+#define NEXT_PAYLOAD_SIZE 1
 
-//#include "iradiocommunication.h"
+#include "iradiocommunication.h"
 
-// class IRadioCommunication;
+//class IRadioCommunication;
 
-  class RF24;
-
-//class RadioCommunication : public IRadioCommunication {
-class RadioCommunication { 
+class RadioCommunication : public IRadioCommunication {
+//class RadioCommunication { 
   private:
     int minPayloadSize;
     int maxPayloadSize;
     int payloadSizeIncrementsBy;
     int nextPayloadSize;
-    char receivePayload[MAX_PAYLOAD_SIZE+1];
-    uint64_t pipeMe;
-    uint64_t pipeOther;
+    char receivePayload[MAX_RCV_PAYLOAD_SIZE+1];
+    uint64_t radioPipes[2];
     // Adapting to this radio
     RF24 radio;
 
   public:
     void setupRadio();
-    RadioCommunication(const uint64_t pipeMe, const uint64_t pipeOther);
+    RadioCommunication(const uint64_t pipes[2]);
     ~RadioCommunication();
     void send(char *sendPayload, uint8_t length); //void send() override;
     uint8_t receive(char *receivePayload); //void receive() override;
