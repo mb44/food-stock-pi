6f81dfa4dcf129308f38b439aab607d96c19650f
root <mortenbeuchert@gmail.com>
2018-05-04 11:17:02 +0000

    Include radiodefines.h

--- foodsupplymonitor/radiocommunication/iradiocommunication.h
+++ foodsupplymonitor/radiocommunication/iradiocommunication.h
@@ -1,16 +1,8 @@
 #ifndef _IRADIO_COMMUNICATION
 #define _IRADIO_COMMUNICATION
 
+#include "radiodefines.h"
 #include <stdint.h>
-
-#define RADIO_RCV_MSG_TYPE_GET_CONTAINER_ID 0
-#define RADIO_RCV_MSG_TYPE_DELETE_CONTAINER 1
-#define RADIO_RCV_MSG_TYPE_MEASUREMENT 2
-
-
-#define RADIO_SEND_MSG_TYPE_SET_CONTAINER_ID 0
-#define RADIO_SEND_MSG_TYPE_POWER_DOWN 1
-#define RADIO_SEND_MSG_TYPE_SET_UPDATE_FREQUENCY 2
 
 class IRadioCommunication {
   public:
