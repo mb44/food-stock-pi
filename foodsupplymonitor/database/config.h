9fede2c5f67fe9f08478c3350e28243d31bb55b8
root <mortenbeuchert@gmail.com>
2018-05-04 10:50:09 +0000

    Remove defines

--- foodsupplymonitor/database/config.h
+++ foodsupplymonitor/database/config.h
@@ -1,15 +1,7 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#include <string>
-
-#define AUTHTOKEN_LENGTH 945
-#define MAX_CONTAINERS 1000
-#define FIREBASE_REPLY_MAX 4096
-
-#define FIREBASECONFIG_LINE_LENGTH_MAX 200
-#define FIREBASECONFIG_VALUE_LENGTH_MAX 150
-
+#include "defines.h"
 
 typedef struct {
   char projectId[FIREBASECONFIG_VALUE_LENGTH_MAX];
