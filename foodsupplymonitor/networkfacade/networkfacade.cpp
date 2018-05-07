fe0130d1b1b17fe9bbda6615d89c3b208c81d08b
root <mortenbeuchert@gmail.com>
2018-05-07 10:19:41 +0000

    Implement handleNetwork method

--- foodsupplymonitor/networkfacade/networkfacade.cpp
+++ foodsupplymonitor/networkfacade/networkfacade.cpp
@@ -1,11 +1,9 @@
 #include "networkfacade.h"
-#include "../database/cjson.h"
 #include <inttypes.h>
 
-NetworkFacade::NetworkFacade(const uint64_t pipes[2]) 
-{
+NetworkFacade::NetworkFacade(const uint64_t pipes[2]) {
   db = new FirebaseAdapter("firebaseConfig.txt");
-  radio = new RadioAdapter(pipes); 
+  radio = new RF24Adapter(pipes); 
 }
 
 NetworkFacade::~NetworkFacade() {
@@ -15,10 +13,34 @@ NetworkFacade::~NetworkFacade() {
 
 void NetworkFacade::handleNetwork() {
   char receivePayload[MAX_RCV_PAYLOAD_SIZE+1];
-  char sendPayload[MAX_SEND_PAYLOAD_SIZE+1];
-  printf("Handle radio communication\n");
+  //char sendPayload[MAX_SEND_PAYLOAD_SIZE+1];
 
   uint8_t length = 0;
+
+  printf("Network facade\n");
+
+
+ /* //db->createContainerItem(13);
+  //db->deleteContainerItem(13);
+  // db->setMeasurement(3, 13.37);
+  //db->setEmptyContainerWeight(3, 5.5);
+  //  db->setMaximumCapacity(3, 11.23);
+  // db->setContainerState(3, "maximumCapacity");
+  //int updateFreq;
+  //db->getUpdateFrequency(3, &updateFreq);
+  //printf("UPDATE FREQ: %d\n", updateFreq);
+  char containerState[30];
+
+  db->getContainerState(3, containerState);
+  printf("CONTAINER STATE: %s\n", containerState);
+
+  if (strcmp(containerState, "measure") == 0) {
+    printf("measuring!!!\n");
+  } else {
+    printf("errrroooo\n");
+}
+*/
+
   // Listen, forever
   while (1) {
     length = radio->receive(receivePayload);
@@ -37,28 +59,22 @@ void NetworkFacade::handleNetwork() {
     uint8_t msgType = receivePayload[0];
     printf("Message type : %d\n", msgType);
 
-    int id = receivePayload[1]<<8 | receivePayload[2];
-    int newContainerId;
+    int scaleId = receivePayload[1]<<8 | receivePayload[2];
     int measurementDecigrams;
     float measurementKilos;
-    char *containerState;
+    char containerState[30];
     int updateFrequencySeconds;
     cJSON *reply;
     switch (msgType) {
-	case RADIO_RCV_MSG_TYPE_GET_CONTAINER_ID:
-          // Get container id from database
-          newContainerId = db->createContainerItem();
-
+	case RADIO_RCV_MSG_TYPE_CREATE_CONTAINER:
+	  // Reply to scale:
 	  // Set message type
+	/*
 	  sendPayload[0] = 0;
 
 	  // Set scale id
-	  sendPayload[1] = receivePayload[2];
+	  sendPayload[1] = receivePayload[2]<<8;
 	  sendPayload[2] = receivePayload[3];
-
-	  // Set new id
-	  sendPayload[3] = newContainerId>>8;
-	  sendPayload[4] = newContainerId;
 
 	  printf("Send payload:\n");
 	  printf("Byte 0: %d\n", sendPayload[0]);
@@ -66,41 +82,39 @@ void NetworkFacade::handleNetwork() {
 	  printf("Byte 2: %d\n", sendPayload[2]);
 	  printf("Byte 3: %d\n", sendPayload[3]);
 	  printf("Byte 4: %d\n", sendPayload[5]);
-
-	  radio->send(sendPayload, MAX_SEND_PAYLOAD_SIZE);
+*/
+	  db->createContainerItem(scaleId);
+	  radio->confirmCreateContainer(scaleId);
           break;
         case RADIO_RCV_MSG_TYPE_DELETE_CONTAINER:
           break;
           // Delete container
+          db->deleteContainerItem(scaleId);
+          radio->powerDown(scaleId);
         case RADIO_RCV_MSG_TYPE_MEASUREMENT:
           // 1. Extract measurement
           measurementDecigrams = receivePayload[3]<<24 | receivePayload[4]<<16 | receivePayload[5]<<8 | receivePayload[6];
           // Convert from decigrams to Kilograms
           measurementKilos = measurementDecigrams / 100.0;
 	  // 2. Get containerState from database
-          reply = db->getContainerState(id);
-          containerState = reply->valuestring;
+          db->getContainerState(scaleId, containerState);
           //printf("Container state: %s\n", containerState);
 
 	  // 3. Set a) Measurement b) emptyContainerWeight 3) maximumCapacity
           if (strcmp(containerState, "measure") == 0) {
-	    db->setMeasurement(id, measurementKilos);
+	    db->setMeasurement(scaleId, measurementKilos);
 	  } else if (strcmp(containerState, "emptyContainerWeight") == 0) {
-	    db->setEmptyContainerWeight(id, measurementKilos);
+	    db->setEmptyContainerWeight(scaleId, measurementKilos);
 	  } else if (strcmp(containerState, "maximumCapacity") == 0) {
-	    db->setMaximumCapacity(id, measurementKilos);
+	    db->setMaximumCapacity(scaleId, measurementKilos);
 	  }
 
 	  // 4. Get update freqency from DB
-          delete containerState;
-          //	cJSON_Delete(reply);
-          reply = db->getUpdateFrequency(id);
-	  //const char *temp = cJSON_Print(reply);
-	  //printf("Reply: %s\n", temp);
-	  updateFrequencySeconds = reply->valueint;
-	  cJSON_Delete(reply);
+          // cJSON_Delete(reply);
+          db->getUpdateFrequency(scaleId, &updateFrequencySeconds);
           printf("Update frequency (seconds): %d\n", updateFrequencySeconds);
 
+/*
           sendPayload[0] = RADIO_SEND_MSG_TYPE_SET_UPDATE_FREQUENCY;
 	  // Add Id
 	  sendPayload[1] = id>>8;
@@ -119,9 +133,10 @@ void NetworkFacade::handleNetwork() {
 	  printf("Byte 4: %d\n: ", sendPayload[4]);
 	  printf("Byte 5: %d\n: ", sendPayload[5]);
 	  printf("Byte 6: %d\n: ", sendPayload[6]);
-
+*/
 	  // 5. Send reply to Scale
-	  radio->send(sendPayload, MAX_SEND_PAYLOAD_SIZE);
+	  radio->setUpdateFrequency(scaleId, updateFrequencySeconds);
+	  //radio->send(sendPayload, MAX_SEND_PAYLOAD_SIZE);
 	  break;
     }
   }
