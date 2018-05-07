f3b25af26f4bec49976e394cf7ac3a1e9afe348d
root <mortenbeuchert@gmail.com>
2018-05-07 10:12:02 +0000

    Added files

---
+++ foodsupplymonitor/radiocommunication/rf24adapter.h
@@ -0,0 +1,35 @@
+#ifndef RF24_ADAPTER_H
+#define RF24_ADAPTER_H
+
+#include "../../RF24.h"
+#include "iradio.h"
+
+#define MIN_PAYLOAD_SIZE 1
+#define MAX_RCV_PAYLOAD_SIZE 7
+#define MAX_SEND_PAYLOAD_SIZE 7
+#define PAYLOAD_SIZE_INCREMENTS_BY 1
+#define NEXT_PAYLOAD_SIZE 1
+
+class RF24Adapter : public IRadio {
+  private:
+    int minPayloadSize;
+    int maxPayloadSize;
+    int payloadSizeIncrementsBy;
+    int nextPayloadSize;
+    uint64_t radioPipes[2];
+    // Adapting to this radio
+    RF24 radio;
+    char receivePayload[MAX_RCV_PAYLOAD_SIZE+1];
+    char sendPayload[MAX_SEND_PAYLOAD_SIZE+1];
+
+  public:
+    void setupRadio();
+    RF24Adapter(const uint64_t pipes[2]);
+    ~RF24Adapter();
+    uint8_t receive(char *receivePayload);
+    void confirmCreateContainer(int scaleId);
+    void powerDown(int scaleId);
+    void setUpdateFrequency(int scaleId, int updateFrequency);
+};
+
+#endif  // RF24_ADAPTER_H 
