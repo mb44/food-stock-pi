e51870477ae72208ad2e9d5332b17b3b3749c25e
root <mortenbeuchert@gmail.com>
2018-05-12 11:48:27 +0000

    Added test files

---
+++ foodsupplymonitor/networkfacade/inetworkfacade.h
@@ -0,0 +1,14 @@
+#ifndef INETWORK_FACADE_H
+#define INETWORK_NACADE_H
+
+#include "../radiocommunication/rf24adapter.h"
+#include "../database/firebaseadapter.h"
+
+class INetworkFacade { 
+   
+  public:
+    virtual ~INetworkFacade() {}
+    virtual void handleNetwork() = 0; 
+};
+
+#endif  // INETWORK_FACADE_H 
