6653e3dbe80c352e33a883dcf702a9838d423a8b
root <mortenbeuchert@gmail.com>
2018-05-13 19:42:45 +0000

    Add IRESTHandler member. Remove method. Edit constructor signature

--- foodsupplymonitor/database/firebaseadapter.h
+++ foodsupplymonitor/database/firebaseadapter.h
@@ -2,7 +2,7 @@
 #define FIREBASEADAPTER_H
 
 #include "idatabase.h"
-#include "auth.h"
+#include "iauth.h"
 #include <string>
 #include <iostream>
 #include "iresthandler.h"
@@ -11,14 +11,15 @@ class FirebaseAdapter : public IDatabase {
   private:
     FirebaseConfig cfg;
     IAuth *auth;
+    IRESTHandler *rest;
     // Authtoken = 945 characters + 1 for null-terminating null-character
     char authToken[AUTHTOKEN_LENGTH+1];
     cJSON *jsonReply;
     void authenticate();
     void parseConfig(const char *firebaseConfig);
-    void executeCURL(char *reply, const char *cmd);
+    // void executeCURL(char *reply, const char *cmd);
   public:
-    FirebaseAdapter(IRESTHandler *rest, const char *firebaseConfig);
+    FirebaseAdapter(IAuth *auth, IRESTHandler *rest, const char *firebaseConfig);
     ~FirebaseAdapter();
     int containerItemExists(int containerId, int *exists) override;
     int createContainerItem(int containerId) override;
