516b1ff21ef7dd37af96a5569091805a6c4be09e
root <mortenbeuchert@gmail.com>
2018-05-13 18:23:18 +0000

    Add tests for packing

--- foodsupplymonitor/tests/tests.cpp
+++ foodsupplymonitor/tests/tests.cpp
@@ -1,53 +1,42 @@
 // tests.cpp
 #include <gtest/gtest.h>
+#include "stubs/radiostub.h"
+#include "../radio/packer.h"
 #include "mytest.cpp"
-#include "radiostub.h"
+#include <cstring>
+
+/**************************
+* Test radio Packing      *
+**************************/
  
+TEST(PackTest, packUpdateFrequency) {
+  Packer p;
+  IPacker *packer = &p;
+  char payload[7] = { 0, 0, 0, 0, 0, 0, 0};
+  int messageType = 1;
+  // 425 = 1A9h
+  int scaleId = 425;
+  // 683 = 2ABh
+  int updateFrequency = 683;
+  packer->pack(payload, scaleId, messageType, updateFrequency);
+
+  char expected[7] =  { messageType, 0x1, 0xA9, 0, 0, 0x2, 0xAB };
+  // Expect comparison of arrays to yield true
+  EXPECT_TRUE(0 == memcmp( expected, payload, 7 ));
+}
+
 TEST(PackTest, packPowerDown) {
-  const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
-  RadioStub radio(pipes);
+  Packer p;
+  IPacker *packer = &p;
+  char payload[7] = { 0, 0, 0, 0, 0, 0, 0};  
   int messageType = 2; 
   int scaleId = 2;
-  radio.pack(scaleId, messageType);
+  packer->pack(payload, scaleId, messageType);
 
-  ASSERT_EQ(messageType, radio.sendPayload[0]);
-  ASSERT_EQ(0, radio.sendPayload[1]);
-  ASSERT_EQ(2, radio.sendPayload[2]);
+  char expected[7] =  { messageType, 0, 2, 0, 0, 0, 0 };
+  // Expect comparison of arrays to yield true
+  EXPECT_TRUE(0 == memcmp( expected, payload, 7 ));
 }
-
-
-TEST(PackTest, packUpdateFrequency) {
-  const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
-  RadioStub radio(pipes);
-  int messageType = 2;
-  // 230 = 139h
-  int scaleId = 313;
-  // 3600 = E10h
-  int updateFrequency = 3600;
-  radio.pack(scaleId, messageType, updateFrequency);
-
-  ASSERT_EQ(messageType, radio.sendPayload[0]);
-  ASSERT_EQ(0x1, radio.sendPayload[1]);
-  ASSERT_EQ(0x39, radio.sendPayload[2]);
-  ASSERT_EQ(0, radio.sendPayload[3]);
-  ASSERT_EQ(0, radio.sendPayload[4]);
-  ASSERT_EQ(0xE, radio.sendPayload[5]);
-  ASSERT_EQ(0x10, radio.sendPayload[6]);
-}
-
-TEST(SquareRootTest, PositiveNos) { 
-    ASSERT_EQ(6, squareRoot(36.0));
-    ASSERT_EQ(18.0, squareRoot(324.0));
-    ASSERT_EQ(25.4, squareRoot(645.16));
-    ASSERT_EQ(0, squareRoot(0.0));
-}
- 
-TEST(SquareRootTest, NegativeNos) {
-    ASSERT_EQ(-1.0, squareRoot(-15.0));
-    ASSERT_EQ(-1.0, squareRoot(-0.2));
-}
-
-
  
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
