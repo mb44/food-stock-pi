044d640ef2ebe5c0d6376b111b496d346b320e95
root <mortenbeuchert@gmail.com>
2018-05-13 18:27:20 +0000

    Add new file

---
+++ foodsupplymonitor/radio/iradio.h
@@ -0,0 +1,16 @@
+#ifndef IRADIO_H
+#define IRADIO_H
+
+#include "radiodefines.h"
+#include <stdint.h>
+
+class IRadio {
+  public:
+    virtual ~IRadio() {}
+    virtual void setUpdateFrequency(int scaleId, int updateFrequency) = 0;
+    virtual void powerDown(int scaleId) = 0;
+    //virtual void send(char *sendPayload, uint8_t length) = 0;
+    virtual uint8_t receive(char *receivePayload) = 0;
+};
+
+#endif // IRADIO_H
