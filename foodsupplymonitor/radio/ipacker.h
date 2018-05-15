2142b87a787b369b02eec968a63aee1269cdcb25
root <mortenbeuchert@gmail.com>
2018-05-15 13:10:05 +0000

    Add unpack method. Change method signatures

--- foodsupplymonitor/radio/ipacker.h
+++ foodsupplymonitor/radio/ipacker.h
@@ -1,11 +1,14 @@
 #ifndef IPACKER_H
 #define IPACKER_H
 
+#include <stdint.h>
+
 class IPacker {
   public:
     virtual ~IPacker() {}
-    virtual void pack(char *payload, int scaleId, int messageType) = 0;
-    virtual void pack(char *payload, int scaleId, int messageType, int updateFrequency) = 0;
+    virtual uint8_t pack(char *payload, const uint8_t scaleId, const int messageType) = 0;
+    virtual uint8_t pack(char *payload, uint8_t scaleId, int messageType, int updateFrequency) = 0;
+    virtual uint8_t unpack(const char *payload, uint8_t *scaleId, int *messageType, int *data) = 0;
 };
 
 #endif // IPACKER_H
