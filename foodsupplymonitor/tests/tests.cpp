906c1211a71a50c579ef45441d9229503f9fc363
root <mortenbeuchert@gmail.com>
2018-05-15 21:04:13 +0000

    Edit tests

--- foodsupplymonitor/tests/tests.cpp
+++ foodsupplymonitor/tests/tests.cpp
@@ -1,5 +1,6 @@
 // tests.cpp
 #include <gtest/gtest.h>
+#include <stdint.h>
 #include "stubs/radiostub.h"
 #include "../radio/packer.h"
 #include "mytest.cpp"
@@ -7,37 +8,58 @@
 
 /**************************
 * Test radio Packing      *
-**************************/
- 
-TEST(PackTest, packUpdateFrequency) {
+**************************/ 
+TEST(PackerTest, packUpdateFrequencyOK) {
   Packer p;
   IPacker *packer = &p;
-  char payload[7] = { 0, 0, 0, 0, 0, 0, 0};
-  int messageType = 1;
+  int payloadSize = 7;
+  char payload[payloadSize] = { 0, 0, 0, 0, 0, 0, 0};
+  uint8_t messageType = 1;
   // 425 = 1A9h
   int scaleId = 425;
   // 683 = 2ABh
   int updateFrequency = 683;
-  packer->pack(payload, scaleId, messageType, updateFrequency);
-
-  char expected[7] =  { messageType, 0x1, 0xA9, 0, 0, 0x2, 0xAB };
+  int success = packer->pack(payload, scaleId, messageType, updateFrequency);
+  char expected[payloadSize] =  { messageType, 0x1, 0xA9, 0, 0, 2, 0xAB };
   // Expect comparison of arrays to yield true
-  EXPECT_TRUE(0 == memcmp( expected, payload, 7 ));
+  EXPECT_TRUE(0 == memcmp( expected, payload, payloadSize ));
 }
 
-TEST(PackTest, packPowerDown) {
+/*
+TEST(PackerTest, packUpdateFrequencyNegativeBAD) {
   Packer p;
   IPacker *packer = &p;
-  char payload[7] = { 0, 0, 0, 0, 0, 0, 0};  
+  int payloadSize = 7;
+  char payload[payloadSize] = { 0, 0, 0, 0, 0, 0, 0};
+  uint8_t messageType = 1;
+  // 425 = 1A9h
+  int scaleId = 425;
+  // 683 = 2ABh
+  int updateFrequency = -25;
+  uint8_t success = packer->pack(payload, scaleId, messageType, updateFrequency);
+
+  char expected[payloadSize] =  { messageType, 0x1, 0xA9, 0, 0, 0x2, 0$
+  // Expect comparison of arrays to yield true
+
+  uint8_t expected = 1;
+  EXPECT_EQ(expected, success);
+}
+*/
+/*
+TEST(PackerTest, packPowerDownOK) {
+  Packer p;
+  IPacker *packer = &p;
+  int payloadSize = 7;
+  char payload[payloadSize] = { 0, 0, 0, 0, 0, 0, 0};  
   int messageType = 2; 
   int scaleId = 2;
   packer->pack(payload, scaleId, messageType);
 
-  char expected[7] =  { messageType, 0, 2, 0, 0, 0, 0 };
+  char expected[payloadSize] =  { messageType, 0, 2, 0, 0, 0, 0 };
   // Expect comparison of arrays to yield true
-  EXPECT_TRUE(0 == memcmp( expected, payload, 7 ));
+  EXPECT_TRUE(0 == memcmp( expected, payload, payloadSize ));
 }
- 
+*/ 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
