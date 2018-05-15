48fd1cb72f9038caaa5d2df6dea46ce94aafb337
root <mortenbeuchert@gmail.com>
2018-05-15 21:00:48 +0000

    Fix method signatures

--- foodsupplymonitor/radio/packer.h
+++ foodsupplymonitor/radio/packer.h
@@ -3,12 +3,12 @@
 
 #include "ipacker.h"
 
-class Packer : public IPacker{
+class Packer : public IPacker {
   public:
     ~Packer();
-    uint8_t pack(char *payload, const uint8_t scaleId, const int messageType) override;
-    uint8_t pack(char *payload, const uint8_t scaleId, const int messageType, const int updateFrequency) override;
-    uint8_t unpack(const char *payload, uint8_t *scaleId, int *messageType, int *data);
+    uint8_t pack(char *payload, const int scaleId, const uint8_t messageType) override;
+    uint8_t pack(char *payload, int scaleId, uint8_t messageType, int updateFrequency) override;
+    uint8_t unpack(const char *payload, int *scaleId, uint8_t *messageType, int *data) override;
 };
 
 #endif // PACKER_H
