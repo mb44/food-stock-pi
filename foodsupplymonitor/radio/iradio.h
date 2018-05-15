f634ad424d3186a17e95e9b8ee1321da76008581
root <mortenbeuchert@gmail.com>
2018-05-15 21:01:35 +0000

    Fix method signatures

--- foodsupplymonitor/radio/iradio.h
+++ foodsupplymonitor/radio/iradio.h
@@ -1,15 +1,16 @@
 #ifndef IRADIO_H
 #define IRADIO_H
 
+#include "../defines.h"
 #include "radiodefines.h"
 #include <stdint.h>
 
 class IRadio {
   public:
     virtual ~IRadio() {}
-    virtual uint8_t setUpdateFrequency(const uint8_t scaleId, const int updateFrequency) = 0;
-    virtual uint8_t powerDown(const uint8_t scaleId) = 0;
-    virtual uint8_t receive(uint8_t *scaleId, int *messageType, int *data) = 0;
+    virtual uint8_t receive(int *scaleId, uint8_t *messageType, int *data) = 0;
+    virtual uint8_t setUpdateFrequency(const int scaleId, const int updateFrequency) = 0;
+    virtual uint8_t powerDown(const int scaleId) = 0;
 };
 
 #endif // IRADIO_H
