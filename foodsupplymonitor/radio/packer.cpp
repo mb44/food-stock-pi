2439eb4e443a1313af962d71342c457611924308
root <mortenbeuchert@gmail.com>
2018-05-15 13:12:11 +0000

    Change method signatures

--- foodsupplymonitor/radio/packer.cpp
+++ foodsupplymonitor/radio/packer.cpp
@@ -3,7 +3,7 @@
 Packer::~Packer() 
 {}
 
-void Packer::pack(char *payload, int scaleId, int messageType, int updateFrequency) {
+uint8_t Packer::pack(char *payload, const uint8_t scaleId, const int messageType, const int updateFrequency) {
   payload[0] = messageType;
   payload[1] = scaleId>>8;
   payload[2] = scaleId;
@@ -11,10 +11,21 @@ void Packer::pack(char *payload, int scaleId, int messageType, int updateFrequen
   payload[4] = updateFrequency>>16;
   payload[5] = updateFrequency>>8;
   payload[6] = updateFrequency;
+  return 0;
 }
 
-void Packer::pack(char *payload, int scaleId, int messageType) {
+uint8_t Packer::pack(char *payload, const uint8_t scaleId, const int messageType) {
   payload[0] = messageType;
   payload[1] = scaleId>>8;
   payload[2] = scaleId;
+  return 0;
+}
+
+uint8_t Packer::unpack(const char *payload, uint8_t *scaleId, int *messageType, int *data) {
+  *messageType = payload[0];
+  *scaleId = payload[2];
+  *scaleId = payload[1]<<8;
+  *data = payload[3]<<24 | payload[4]<<16 | payload[5]<<8 | payload[6]; 
+
+  return 0;
 }
