2d32420a5b6be80bef2cc0f0dfaf726ab70ec7e1
root <mortenbeuchert@gmail.com>
2018-05-15 13:10:42 +0000

    Change method signatures

--- foodsupplymonitor/radio/iradio.h
+++ foodsupplymonitor/radio/iradio.h
@@ -7,10 +7,9 @@
 class IRadio {
   public:
     virtual ~IRadio() {}
-    virtual void setUpdateFrequency(int scaleId, int updateFrequency) = 0;
-    virtual void powerDown(int scaleId) = 0;
-    //virtual void send(char *sendPayload, uint8_t length) = 0;
-    virtual uint8_t receive(char *receivePayload) = 0;
+    virtual uint8_t setUpdateFrequency(const uint8_t scaleId, const int updateFrequency) = 0;
+    virtual uint8_t powerDown(const uint8_t scaleId) = 0;
+    virtual uint8_t receive(uint8_t *scaleId, int *messageType, int *data) = 0;
 };
 
 #endif // IRADIO_H
