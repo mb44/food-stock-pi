e7dbf2574b1bbfe76c0b312e10e12806a811b871
root <mortenbeuchert@gmail.com>
2018-05-16 18:41:16 +0000

    Update method signature

--- foodsupplymonitor/database/idatabase.h
+++ foodsupplymonitor/database/idatabase.h
@@ -10,15 +10,14 @@
 class IDatabase {
   public:
     virtual ~IDatabase() {}
-    virtual int containerItemExists(int, int*) = 0;
-    virtual int createContainerItem(int) = 0;
-    virtual int deleteContainerItem(int) = 0;
-    virtual int setCurrentAmount(int, float) = 0;
-    virtual int setEmptyContainerWeight(int, float) = 0;
-    virtual int setMaximumCapacity(int, float) = 0;
-    virtual int getContainerState(int, char*) = 0;
-    virtual int setContainerState(int, const char*) = 0;
-    virtual int getUpdateFrequency(int, int*) = 0;
+    virtual uint8_t containerItemExists(int, int*) = 0;
+    virtual uint8_t createContainerItem(int) = 0;
+    virtual uint8_t setCurrentAmount(int, float) = 0;
+    virtual uint8_t setEmptyContainerWeight(int, float) = 0;
+    virtual uint8_t setMaximumCapacity(int, float) = 0;
+    virtual uint8_t getContainerState(int, char*) = 0;
+    virtual uint8_t setContainerState(int, const char*) = 0;
+    virtual uint8_t getUpdateFrequency(int, int*) = 0;
 };
 
 #endif
