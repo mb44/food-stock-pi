d185b27787058396f6b2aecf26c4efddbb333726
root <mortenbeuchert@gmail.com>
2018-06-01 12:57:59 +0000

    Add Doxygen

--- foodsupplymonitor/html/search/searchdata.js
+++ foodsupplymonitor/html/search/searchdata.js
@@ -1,21 +1,24 @@
 var indexSectionsWithContent =
 {
-  0: "fr",
+  0: "fpr",
   1: "f",
-  2: "r"
+  2: "p",
+  3: "r"
 };
 
 var indexSectionNames =
 {
   0: "all",
   1: "files",
-  2: "pages"
+  2: "variables",
+  3: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
   1: "Files",
-  2: "Pages"
+  2: "Variables",
+  3: "Pages"
 };
 
